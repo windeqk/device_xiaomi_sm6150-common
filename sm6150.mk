ebf97630fe1ffc7b3897c652e67e96f38df6df5d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-12 15:45:10 +0530

    sm6150-common: rootdir: Import USB init scripts from stock
     * fixes mtp dead on windows until drivers are reinstalled
     * remove debug script exec while we're at it to fix init crash
    
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -205,6 +205,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/bin/init.mi.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.mi.usb.sh \
+    $(LOCAL_PATH)/rootdir/bin/init.qcom.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.usb.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh

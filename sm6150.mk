ca1c5189e366c317d6dc491d0e85705651c74e57
sashascurtu <sasha.scurtu@gmail.com>
2021-06-29 00:30:52 +0700

    sm6150-common: Build power stats HAL and label it
    Signed-off-by: agustkid <agustkid@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -281,7 +281,8 @@ PRODUCT_USES_QCOM_HARDWARE := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 PRODUCT_PACKAGES += \
